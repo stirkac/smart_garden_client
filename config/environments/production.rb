68481ae1a50a9754b3c1e41d519a719fc6105f92
stirkac <sebastjan.stojnsek@gmail.com>
2015-08-10 22:36:13 +0200

    Improvements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  config.action_mailer.default_url_options = { host: 'http://smart-garden-client.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://garden-stirkac.rhcloud.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
