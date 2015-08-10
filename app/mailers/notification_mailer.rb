c0f0e810897b9a225d46f783d4a2a85524a1d433
stirkac <sebastjan.stojnsek@gmail.com>
2015-08-10 17:18:35 +0200

    Perhaps fixed mailer for ruby 2.0

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,13 @@
 class NotificationMailer < ActionMailer::Base
   default :to => (ENV['LOGGER_MAIL'])
 
-  def status_notification_email(grow:, status:)
+  def status_notification_email(grow: nil, status: nil)
   	@status=status
   	@grow=grow
     mail(to: grow.user.email, from: ENV["EMAIL"], subject: "Notification from your garden #{grow.name}")
   end
 
-  def scheduled_notification_email(grow:, schedule:)
+  def scheduled_notification_email(grow: nil, schedule: nil)
   	@schedule=schedule
   	@grow=grow
   	mail(to: grow.user.email, from: ENV["EMAIL"], subject: "Scheduled event from your garden #{grow.name}")
