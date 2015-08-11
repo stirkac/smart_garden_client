8a95b3e8e05747c77d39b19445fcabfaf3e6dd48
stirkac <sebastjan.stojnsek@gmail.com>
2015-08-11 15:17:48 +0200

    Worker fix

--- app/workers/status_notifier.rb
+++ app/workers/status_notifier.rb
@@ -7,16 +7,20 @@ class StatusNotifier
 
   def perform options={}
     Grow.not_updated.each do |grow|
-      current_status = HTTParty.get(grow.api_location + "/current")
-      if current_status.code < 400
-        unless (current_status["humidity"].between?(grow.hum_low, grow.hum_high) and current_status["temperature"].between?(grow.temp_low, grow.temp_high))
-          status=Status.new current_status
-          unless Status.exists?(status.id)
-            if status.save!
-              Notification.new(grow: grow, status: status, dismissed: false, content: "params_not_in_range").save!
+      begin
+        current_status = HTTParty.get(grow.api_location + "/current.json")
+        if current_status.code < 400
+          unless (current_status["humidity"].between?(grow.hum_low, grow.hum_high) and current_status["temperature"].between?(grow.temp_low, grow.temp_high))
+            status=Status.new current_status
+            unless Status.exists?(status.id)
+              if status.save!
+                Notification.new(grow: grow, status: status, dismissed: false, content: "params_not_in_range").save!
+              end
             end
           end
         end
+      rescue
+        logger.warn("Job error: Error fetching current status: Grow##{grow.id} (#{grow.api_location})");
       end
     end
   end
