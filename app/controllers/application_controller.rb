c9b9fd82b8ab96428dc82fc254520d2a49666877
stirkac <sebastjan.stojnsek@gmail.com>
2015-08-15 20:16:03 +0200

    Mailer fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-  	grows_path
+  	if resource.is_a? AdminUser
+  		admin_root_path
+  	else
+  		resource.grows.count != 1 ? grows_path : grow_path(resource.grows.first)
+  	end
   end
 end
